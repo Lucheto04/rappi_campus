4c4da8fe2784ebe538245a8e7c64033225191a7a
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-22 21:14:11 -0500

    feat: :sparkles: Creamos la coneccion a atlas

---
+++ db/atlas.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+dotenv.config("../");
+
+export async function coneccion() {
+    try {
+        const uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@cluster0.nhhfhmc.mongodb.net/${process.env.ATLAS_DB}`
+        const options = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        };
+        const client = await MongoClient.connect(uri, options);
+        return client.db();
+    } catch (error) {
+        return {status: 500, message: error};
+    }
+}
