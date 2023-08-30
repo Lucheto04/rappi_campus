08de15c8a91c588daa73b102ddb456183a06db37
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-29 23:00:53 -0500

    refactor: :ambulance: Carregimos el dto
    Cambiamos la validacion de check a body y cambiamos las variables a ingles

--- app/middlewares/secure/usuarios.js
+++ app/middlewares/secure/usuarios.js
@@ -1,38 +1,37 @@
-import { check } from "express-validator";
+import { body } from "express-validator";
 
 let trainer = {
-    entidad1: "usuario",
-    entidad2: "nombre",
-    entidad3: "correo",
-    entidad4: "contraseña",
-    entidad5: "telefono",
-    entidad6: "nivel"
+    entidad1: "user",
+    entidad2: "name",
+    entidad3: "email_user",
+    entidad4: "password_user",
+    entidad5: "movil_number",
+    entidad6: "level"
 }
 let {entidad1: u1, entidad2: u2, entidad3: u3, entidad4: u4, entidad5: u5, entidad6: u6} = trainer
 
-export const postUsuarioDtoV1 = () => [
-    check(`${u1}`)
-    .notEmpty().withMessage(`El ${t1} es obligatorio.`)
-    .isString().withMessage(`El ${t1} debe ser un string.`),
+export const postUsuarioDto = [
+    body(`${u1}`)
+    .notEmpty().withMessage(`El ${u1} es obligatorio.`)
+    .isString().withMessage(`El ${u1} debe ser un string.`),
 
-    check(`${u2}`)
-    .notEmpty().withMessage(`El ${t1} es obligatorio.`)
-    .isString().withMessage(`El ${t1} debe ser un string.`)
-    .matches(/^[a-zA-Z]+$/).withMessage(`El ${t1} solo resive letras.`),
+    body(`${u2}`)
+    .notEmpty().withMessage(`El ${u2} es obligatorio.`)
+    .isString().withMessage(`El ${u2} debe ser un string.`),
 
-    check(`${u3}`)
-    .notEmpty().withMessage(`El ${t1} es obligatorio.`)
-    .isString().withMessage(`El ${t1} debe ser un string.`),
+    body(`${u3}`)
+    .notEmpty().withMessage(`El ${u3} es obligatorio.`)
+    .isString().withMessage(`El ${u3} debe ser un string.`),
 
-    check(`${u4}`)
-    .notEmpty().withMessage(`El ${t1} es obligatorio.`)
-    .isString().withMessage(`El ${t1} debe ser un string.`),
+    body(`${u4}`)
+    .notEmpty().withMessage(`El ${u4} es obligatorio.`)
+    .isString().withMessage(`El ${u4} debe ser un string.`),
 
-    check(`${u5}`)
-    .notEmpty().withMessage(`El ${t1} es obligatorio.`)
-    .isString().withMessage(`El ${t1} debe ser un string.`),
+    body(`${u5}`)
+    .notEmpty().withMessage(`El ${u5} es obligatorio.`)
+    .isString().withMessage(`El ${u5} debe ser un string.`),
 
-    check(`${u6}`)
-    .notEmpty().withMessage(`El ${i1} es obligatorio.`)
-    .isNumeric().withMessage(`El ${i1} debe ser el numero del id de un rol.`),
+    body(`${u6}`)
+    .notEmpty().withMessage(`El ${u6} es obligatorio.`)
+    .isInt().withMessage(`El ${u6} debe ser el numero del id de un rol.`),
 ]
