498b97d4387510aaf19e0dc93974e425dfac0434
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-30 06:43:53 -0500

    feat: :sparkles: Creamos delete para producto

--- app/controllers/producto.js
+++ app/controllers/producto.js
@@ -62,3 +62,21 @@ export const putProducto = async (req, res) => {
         res.status(500).json({ status: 500, message: "Couldnt connect to the database :C" });
     }
 }
+
+export const deleteProducto = async (req, res) => {
+    try {
+        // Capture 'id' from req.query
+        const {id} = req.query
+        
+        const result = await producto.deleteOne(
+            {id_producto: parseInt(id)}
+        )
+        if(result.deletedCount === 0) return res.status(404).send('that producto does not exist in the database');
+        
+        // Res consult...
+        res.status(200).json({status:200,message:'deleted successfully 🙃'});
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({status:404,message:"Couldn't delete that 'usuario'"})
+    }
+}
--- app/routes/producto.routes.js
+++ app/routes/producto.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getProductos, postProducto, putProducto } from "../controllers/producto.js";
+import { getProductos, postProducto, putProducto, deleteProducto } from "../controllers/producto.js";
 import { productoDto } from "../middlewares/secure/productos.js";
 // import routesVersioning from "express-routes-versioning";
 
@@ -8,6 +8,7 @@ const appProducto = Router();
 appProducto.get('/', getProductos);
 appProducto.post('/', productoDto, postProducto);
 appProducto.put('/', productoDto, putProducto);
+appProducto.delete('/', deleteProducto);
 
 
 export default appProducto;
