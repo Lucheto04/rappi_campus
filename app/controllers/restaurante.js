99df209799ec69de2cee5d6d0261f0a2e3094396
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-29 23:56:29 -0500

    feat: :sparkles: Creamos post de restaurantes

--- app/controllers/restaurante.js
+++ app/controllers/restaurante.js
@@ -12,3 +12,31 @@ export const getRestaurante = async (req, res) => {
         res.status(500).json({ status: 500, message: "Couldnt connect to the database :C" })
     }
 }
+
+
+export const postRestaurante = async (req, res,) => {
+    try {
+        const id = await siguienteId('restaurantes');
+
+        // Validation
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+        
+        // change of variables
+        const {name: nombre, address: direccion, qualification: calificacion } = req.body
+        const json = Object.assign({nombre, direccion, calificacion})
+        
+        // New document 
+        const newDocument = {
+            id_restaurante: id,
+            ...json
+        }
+        await restaurante.insertOne(newDocument);
+
+        // Res consult...
+        res.status(201).json({status:201,message:"'restaurante' added successfully 😀"});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ status: 500, message: "Couldnt connect to the database :C" });
+    }
+}
--- app/routes/restaurante.routes.js
+++ app/routes/restaurante.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { getRestaurante } from "../controllers/restaurante.js";
+import { getRestaurante, postRestaurante } from "../controllers/restaurante.js";
+import { postRestauranteDto } from "../middlewares/secure/restaurantes.js";
+// import routesVersioning from "express-routes-versioning";
 
 const appRestaurante = Router();
 
 appRestaurante.get('/', getRestaurante);
+appRestaurante.post('/', postRestauranteDto, postRestaurante);
 
 
 export default appRestaurante;
