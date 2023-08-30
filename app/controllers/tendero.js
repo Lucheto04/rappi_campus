de0d79d4f05a103bb65b1fcfdaf4b7aabe426cae
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-30 08:43:40 -0500

    feat: :sparkles: Creamos post de tenderos

--- app/controllers/tendero.js
+++ app/controllers/tendero.js
@@ -12,3 +12,37 @@ export const getTendero = async (req, res) => {
         res.status(500).json({ status: 500, message: "Couldnt connect to the database :C" })
     }
 }
+// {
+//     "_id": "64eeb14c586f9929fd87f609",
+//     "id_tendero": 1,
+//     "nombre_completo": "Pancho Gerardo Martinez Martinez",
+//     "email": "pancho@gmail.com",
+//     "password": "panchit0001",
+//     "movil": "+573156794374"
+// }
+export const postTendero = async (req, res,) => {
+    try {
+        const id = await siguienteId('rappi_tendero');
+
+        // Validation
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+        
+        // change of variables
+        const {name: nombre_completo, email_tendero: email, password_tendero: password, movil_number: movil } = req.body;
+        const json = Object.assign({nombre_completo, email, password, movil});
+        
+        // New document 
+        const newDocument = {
+            id_tendero: id,
+            ...json
+        }
+        await tendero.insertOne(newDocument);
+
+        // Res consult...
+        res.status(201).json({status:201,message:"'tendero' added successfully 😀"});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ status: 500, message: "Couldnt connect to the database :C" });
+    }
+}
--- app/routes/tendero.routes.js
+++ app/routes/tendero.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getTendero } from "../controllers/tendero.js";
-
+import { getTendero, postTendero } from "../controllers/tendero.js";
+import { tenderoDto } from "../middlewares/secure/tenderos.js";
 // import routesVersioning from "express-routes-versioning";
 
 const appTendero = Router();
 
 appTendero.get('/', getTendero);
+appTendero.post('/', tenderoDto, postTendero);
 
 
 export default appTendero;
