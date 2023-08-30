b4a5885f88535a2a7f1b71f596422fb67da5fc65
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-08-30 00:17:34 -0500

    feat: :sparkles: DTO y post de cupones

--- app/controllers/cupones.controller.js
+++ app/controllers/cupones.controller.js
@@ -1,9 +1,37 @@
-import { conexion } from "../../config/connection/atlas.js";
+import { collectionGen } from "../../config/connection/atlas.js";
+import { validationResult } from "express-validator";
+import { siguienteId } from "../helpers/siguienteId.js";
 
-const db = await conexion();
-const cupones = db.collection('cupones') 
+const cupones = await collectionGen('cupones') 
 
-export const getAllCupones = async (req,res) => {
+const getAllCupones = async (req,res) => {
     const result = await cupones.find().toArray();
     res.send(result);
 }
+const postCupones = async (req, res) => {
+    //Validacion 
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) return res.status(422).send(errors);
+    try {
+        //Desestructuracion  y autoincrement
+        let { cupon, expiration, discount, validation } = req.body
+
+        const date = new Date(expiration)
+        const idCupon = await siguienteId('cupones');
+
+        const newParams = {
+            id_cupon: idCupon,
+            cupon: cupon,
+            expiracion: date,
+            descuento: discount,
+            valido: validation
+        }
+        await cupones.insertOne(newParams)
+        res.status(201).json({status:201,message:'Cupon insertado correctamente :D'})
+    } catch (error) {
+        res.send(error)
+    }
+
+}
+
+export{getAllCupones,postCupones}
--- app/routes/cupones.routes.js
+++ app/routes/cupones.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import {getAllCupones} from '../controllers/cupones.controller.js'
+import {getAllCupones, postCupones} from '../controllers/cupones.controller.js'
+import {cuponesDTO} from "../middlewares/secure/cupones.dto.js"
 const appCupones = Router();
 
 appCupones.get('/', getAllCupones);
+appCupones.post('/',cuponesDTO,postCupones);
 
 export default appCupones;
