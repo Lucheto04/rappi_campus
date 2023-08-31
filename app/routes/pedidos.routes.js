34316d427d3b68487969c08d8516dbb9f32b63e2
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-08-31 08:20:52 -0500

    feat: :safety_vest: Agregar DTO para la coleccion pedido

--- app/routes/pedidos.routes.js
+++ app/routes/pedidos.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getAllPedidos, postPedidos } from "../controllers/pedidos.controller.js";
+import { pedidosDTO } from "../middlewares/secure/pedidos.dto.js";
 
 const appPedidos = Router() 
 
 appPedidos.get('/', getAllPedidos);
-appPedidos.post('/',postPedidos)
+appPedidos.post('/',pedidosDTO,postPedidos)
 
 export default appPedidos;
