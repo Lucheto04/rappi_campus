0f6400fccd03de24a443ad149756c5cf99497b4a
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-09-02 19:33:32 -0500

    feat: :wheelchair: Incluir rateLimit a tenderos

--- app/routes/tendero.routes.js
+++ app/routes/tendero.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { getTendero, postTendero, putTendero, deleteTendero } from "../controllers/tendero.js";
 import { tenderoDto } from "../middlewares/secure/tenderos.js";
+import { limitReq } from "../middlewares/rateLimit.js";
 // import routesVersioning from "express-routes-versioning";
 
 const appTendero = Router();
+
+appTendero.use(limitReq());
 
 appTendero.get('/', getTendero);
 appTendero.post('/', tenderoDto, postTendero);
