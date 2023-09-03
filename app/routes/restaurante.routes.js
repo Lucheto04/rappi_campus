2fe8f295f864f5e0986420dbbdc7e4e7cb1c377c
Lucheto04 <lucho.lgrg04@gmail.com>
2023-09-03 00:58:11 -0500

    feat: :sparkles: Creamos las primeras 3 consultas de la V2

--- app/routes/restaurante.routes.js
+++ app/routes/restaurante.routes.js
@@ -2,13 +2,29 @@ import { Router } from "express";
 import { getRestaurante, postRestaurante, putRestaurante, deleteRestaurante } from "../controllers/restaurante.js";
 import { postRestauranteDto } from "../middlewares/secure/restaurantes.js";
 import { limitReq } from "../middlewares/rateLimit.js";
-// import routesVersioning from "express-routes-versioning";
-
+import routesVersioning from "express-routes-versioning";
+import { restaurantByName, restaurantByQualification, restaurantWithProducts } from "../versions/V2/restaurantes.js";
 const appRestaurante = Router();
-
+const version = routesVersioning();
 appRestaurante.use(limitReq());
 
-appRestaurante.get('/', getRestaurante);
+// Busquedas de restaurantes de la V1 y V2, en la V2 se filtra los restaurantes por nombre pasandolo en el 'body' de la consulta.
+appRestaurante.get('/', version({
+    "1.0.0": getRestaurante,
+    "2.0.0": restaurantByName
+}));
+
+// Busqueda de restaurantes con todos sus productos de la V2.
+appRestaurante.get('/products', version({
+    "2.0.0": restaurantWithProducts
+}));
+
+// Busqueda de restaurantes por la calificacion dentro de los parametros de la V2.
+appRestaurante.get('/:qualification', version({
+    "2.0.0": restaurantByQualification
+}));
+
+
 appRestaurante.post('/', postRestauranteDto, postRestaurante);
 appRestaurante.put('/', postRestauranteDto, putRestaurante);
 appRestaurante.delete('/', deleteRestaurante);
