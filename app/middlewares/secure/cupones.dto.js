4da0144c5c1701e63bf1d5387beaa78918167d02
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-09-04 13:26:17 -0500

    feat: :adhesive_bandage: Emparejar DTO con los de la DB

--- app/middlewares/secure/cupones.dto.js
+++ app/middlewares/secure/cupones.dto.js
@@ -5,16 +5,16 @@ export const cuponesDTO = [
         .notEmpty().withMessage('El cupon es obligatorio')
         .isString().withMessage('El cupon debe ser string'),
     body('expiration')
-        .notEmpty().withMessage('La expiracion es obligatoria')
-        .isString().withMessage('La expiracion debe ser string y cumplir este formato "YYYY-MM-DDTHH:mm:ssZ"')
+        .notEmpty().withMessage('La expiration es obligatoria')
+        .isString().withMessage('La expiration debe ser string y cumplir este formato "YYYY-MM-DDTHH:mm:ssZ"')
         .matches(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/).withMessage('La expiracion debe tener formato "YYYY-MM-DDTHH:mm:ssZ"'),
     body('discount')
-        .notEmpty().withMessage('El descuento es obligatorio')
-        .isNumeric().withMessage('El descuento debe ser tipo numérico'),
+        .notEmpty().withMessage('El discount es obligatorio')
+        .isNumeric().withMessage('El discount debe ser tipo numérico'),
     body('validation')
-        .notEmpty().withMessage('El valido es obligatorio')
-        .isBoolean().withMessage('El valido debe ser tipo boolean'),
+        .notEmpty().withMessage('El validation es obligatorio')
+        .isBoolean().withMessage('El validation debe ser tipo boolean'),
     body('id_ConsumeUser')
         .optional()
-        .isNumeric().withMessage('El id_usuario_utiliza debe ser tipo numérico')
+        .isNumeric().withMessage('El id_ConsumeUser debe ser tipo numérico')
 ]
