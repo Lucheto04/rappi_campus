e76a5d21da6dc00169b9557c3f9942e9dcb0bb44
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-09-03 19:55:08 -0500

    feat: :passport_control: JWT para las colecciones
    Este Token valida que este permitido, el endpoint, la version y el método

--- app/routes/cupones.routes.js
+++ app/routes/cupones.routes.js
@@ -2,9 +2,10 @@ import { Router } from "express";
 import {deleteCupones, getAllCupones, postCupones, putCupones} from '../controllers/cupones.controller.js'
 import {cuponesDTO} from "../middlewares/secure/cupones.dto.js"
 import { limitReq } from "../middlewares/rateLimit.js";
+import { verifyToken } from "../middlewares/jwt.js";
 const appCupones = Router();
 
-appCupones.use(limitReq())
+appCupones.use(verifyToken,limitReq())
 
 appCupones.get('/', getAllCupones);
 appCupones.post('/', cuponesDTO, postCupones);
--- app/routes/pedidos.routes.js
+++ app/routes/pedidos.routes.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { deletePedidos, getAllPedidos, postPedidos, putPedidos } from "../controllers/pedidos.controller.js";
 import { pedidosDTO } from "../middlewares/secure/pedidos.dto.js";
 import { limitReq } from "../middlewares/rateLimit.js";
+import { verifyToken } from "../middlewares/jwt.js";
 
 const appPedidos = Router() 
 
-appPedidos.use(limitReq());
+appPedidos.use(verifyToken,limitReq());
 
 appPedidos.get('/', getAllPedidos);
 appPedidos.post('/', pedidosDTO, postPedidos);
